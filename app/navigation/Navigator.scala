e1dff6068d3d3273d9519d8800cffd4e33236643
“JoshuaH01” <34240122+JoshuaH01@users.noreply.github.com>
2020-12-18 12:46:42 +0000

    TRUS-3186-2| WIP | Moved agent routes over

---
+++ app/navigation/Navigator.scala
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package navigation
+
+import javax.inject.{Inject, Singleton}
+import navigation.routes._
+import play.api.mvc.Call
+import uk.gov.hmrc.auth.core.AffinityGroup
+
+@Singleton
+class Navigator @Inject()(
+                         config: FrontendAppConfig
+                         ) {
+
+  private def defaultRoute: PartialFunction[Page, AffinityGroup => UserAnswers => Call] = {
+    case _ => _ => _ => controllers.register.routes.IndexController.onPageLoad()
+  }
+
+  protected def route(draftId: String): PartialFunction[Page, AffinityGroup => UserAnswers => Call] =
+    AgentRoutes.route(draftId) orElse
+      AssetsRoutes.route(draftId) orElse
+      MatchingRoutes.route(draftId, config) orElse
+      SuitabilityRoutes.route(draftId) orElse
+      defaultRoute
+
+  def nextPage(page: Page, mode: Mode = NormalMode, draftId: String, af :AffinityGroup = AffinityGroup.Organisation): UserAnswers => Call = mode match {
+    case NormalMode =>
+      route(draftId)(page)(af)
+    case CheckMode =>
+      route(draftId)(page)(af)
+  }
+
+}
