ca00a716d3f241fd13b7b2c85d4fb73418c60eef
jassalrichy <20478717+jassalrichy@users.noreply.github.com>
2021-08-10 12:15:12 +0100

    Fix Text Area to mimic Input Text

--- app/assets/javascripts/registertrustagentdetailsfrontend.js
+++ app/assets/javascripts/registertrustagentdetailsfrontend.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
 
+$("textarea").keydown(function(e){
+    // Enter was pressed
+    if (e.keyCode == 13)
+    {
+        // prevent default behavior
+        e.preventDefault();
+    }
+});
+
 });
