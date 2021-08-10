8c5575b9cf40823877929525e0b985149a259a06
CarolineS-QA <62263396+CarolineS-QA@users.noreply.github.com>
2021-08-10 10:01:09 +0100

    TRUS-4077 [CS] upgrade to play 2.8

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,22 +5,20 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "org.reactivemongo" %% "play2-reactivemongo"            % "0.20.13-play27",
-    "uk.gov.hmrc"       %% "play-health"                    % "3.16.0-play-27",
-    "uk.gov.hmrc"       %% "play-frontend-hmrc"             % "0.79.0-play-27",
-    "uk.gov.hmrc"       %% "play-conditional-form-mapping"  % "1.6.0-play-27",
-    "uk.gov.hmrc"       %% "bootstrap-frontend-play-27"     % "5.8.0",
-    "uk.gov.hmrc"       %% "domain"                         % "5.10.0-play-27",
-    "com.typesafe.play" %% "play-json-joda"                 % "2.7.4",
-    "org.typelevel"     %% "cats-core"                      % "2.0.0",
-    "uk.gov.hmrc"       %% "tax-year"                       % "1.1.0",
-    "uk.gov.hmrc"       %% "play-language"                  % "5.1.0-play-27"
+    "org.reactivemongo"   %% "play2-reactivemongo"            % "0.20.13-play28",
+    "uk.gov.hmrc"         %% "play-frontend-hmrc"             % "0.90.0-play-28",
+    "uk.gov.hmrc"         %% "play-conditional-form-mapping"  % "1.9.0-play-28",
+    "uk.gov.hmrc"         %% "domain"                         % "6.2.0-play-28",
+    "uk.gov.hmrc"         %% "bootstrap-frontend-play-28"     % "5.9.0",
+    "com.typesafe.play"   %% "play-json-joda"                 % "2.7.4",
+    "org.typelevel"       %% "cats-core"                      % "2.0.0",
+    "uk.gov.hmrc"         %% "tax-year"                       % "1.1.0"
   )
 
   val test: Seq[ModuleID] = Seq(
     "org.pegdown"              % "pegdown"                % "1.6.0",
-    "org.scalatest"           %% "scalatest"              % "3.0.8",
-    "org.scalatestplus.play"  %% "scalatestplus-play"     % "4.0.3",
+    "org.scalatest"           %% "scalatest"              % "3.0.9",
+    "org.scalatestplus.play"  %% "scalatestplus-play"     % "5.0.0",
     "org.jsoup"                % "jsoup"                  % "1.12.1",
     "com.typesafe.play"       %% "play-test"              % PlayVersion.current,
     "org.mockito"              % "mockito-all"            % "1.10.19",
@@ -35,11 +33,11 @@ object AppDependencies {
   val akkaHttpVersion = "10.1.12"
 
   val overrides = Seq(
-    "com.typesafe.akka" %% "akka-stream" % akkaVersion,
-    "com.typesafe.akka" %% "akka-protobuf" % akkaVersion,
-    "com.typesafe.akka" %% "akka-slf4j" % akkaVersion,
-    "com.typesafe.akka" %% "akka-actor" % akkaVersion,
-    "com.typesafe.akka" %% "akka-http-core" % akkaHttpVersion,
+    "com.typesafe.akka" %% "akka-stream_2.12" % akkaVersion,
+    "com.typesafe.akka" %% "akka-protobuf_2.12" % akkaVersion,
+    "com.typesafe.akka" %% "akka-slf4j_2.12" % akkaVersion,
+    "com.typesafe.akka" %% "akka-actor_2.12" % akkaVersion,
+    "com.typesafe.akka" %% "akka-http-core_2.12" % akkaHttpVersion,
     "commons-codec"     % "commons-codec" % "1.12"
   )
 }
