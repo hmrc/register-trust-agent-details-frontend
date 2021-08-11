258967ad6b8423c03a6266ac35172abee75d1c65
jassalrichy <20478717+jassalrichy@users.noreply.github.com>
2021-08-11 12:42:46 +0100

    tidy up

--- app/assets/javascripts/registertrustagentdetailsfrontend.js
+++ app/assets/javascripts/registertrustagentdetailsfrontend.js
@@ -1,14 +1,3 @@
 $(document).ready(function() {
 
-$("textarea").keydown(function(e){
-    // Shift + Enter was pressed
-    if (e.keyCode == 13 && e.shiftKey)
-    {
-        e.preventDefault();
-    } else if (e.keyCode == 13) {
-        e.preventDefault();
-        $(".govuk-button").trigger("click");
-    }
-});
-
 });
