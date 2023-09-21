39a4cb66385b92ade2ce2fd2c9db4309d26aa870
ina-tu <74660663+ina-tu@users.noreply.github.com>
2023-09-21 10:22:15 +0100

    DDCE-4614: updated character count component

--- app/views/ViewUtils.scala
+++ app/views/ViewUtils.scala
@@ -18,7 +18,10 @@ package views
 
 import play.api.data.{Form, FormError}
 import play.api.i18n.Messages
+import play.twirl.api.{Html, HtmlFormat}
 import viewmodels.RadioOption
+
+import scala.collection.immutable
 
 object ViewUtils {
 
@@ -47,4 +50,14 @@ object ViewUtils {
         }
     }
   }
+
+  //  Copied over from the play-frontend-hmrc view utils
+
+  private[views] def govukPluralisedI18nAttributes(
+                                                    translationKey: String,
+                                                    pluralForms: Option[Map[String, String]]
+                                                  ): immutable.Iterable[Html] =
+    pluralForms.getOrElse(Map.empty).map { case (k, v) =>
+      Html(s"""data-i18n.$translationKey.${HtmlFormat.escape(k)}="${HtmlFormat.escape(v)}" """)
+    }
 }
--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,11 +2,11 @@ import sbt.*
 
 object AppDependencies {
 
-  val bootstrapVersion = "7.21.0"
+  val bootstrapVersion = "7.22.0"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"         %% "play-frontend-hmrc"             % "7.19.0-play-28",
+    "uk.gov.hmrc"         %% "play-frontend-hmrc"             % "7.20.0-play-28",
     "uk.gov.hmrc"         %% "play-conditional-form-mapping"  % "1.13.0-play-28",
     "uk.gov.hmrc"         %% "domain"                         % "8.3.0-play-28",
     "uk.gov.hmrc"         %% "bootstrap-frontend-play-28"     % bootstrapVersion,
@@ -16,12 +16,12 @@ object AppDependencies {
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"             %% "bootstrap-test-play-28"   % bootstrapVersion,
-    "org.scalatest"           %% "scalatest"                % "3.2.16",
-    "org.scalatestplus"       %% "scalacheck-1-17"          % "3.2.16.0",
+    "org.scalatest"           %% "scalatest"                % "3.2.17",
+    "org.scalatestplus"       %% "scalacheck-1-17"          % "3.2.17.0",
     "org.jsoup"                % "jsoup"                    % "1.16.1",
-    "org.mockito"             %% "mockito-scala-scalatest"  % "1.17.14",
+    "org.mockito"             %% "mockito-scala-scalatest"  % "1.17.27",
     "org.scalacheck"          %% "scalacheck"               % "1.17.0",
-    "com.github.tomakehurst"   % "wiremock-standalone"      % "2.27.2",
+    "org.wiremock"             % "wiremock-standalone"      % "3.2.0",
     "wolfendale"              %% "scalacheck-gen-regexp"    % "0.1.2",
     "com.vladsch.flexmark"     % "flexmark-all"             % "0.64.8"
   ).map(_ % Test)
