627ab994b28f5a3cf46d6c3c0c6eb25c4b6be35a
maheshhvr <175945803+maheshhvr@users.noreply.github.com>
2025-07-03 10:18:24 +0100

    DDCe-5929 -Migrate away from mockito-scala-scalatest

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,7 +6,7 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"   %% "play-frontend-hmrc-play-30"            % "12.6.0",
+    "uk.gov.hmrc"   %% "play-frontend-hmrc-play-30"            % "12.7.0",
     "uk.gov.hmrc"   %% "bootstrap-frontend-play-30"            % bootstrapVersion,
     "uk.gov.hmrc"   %% "play-conditional-form-mapping-play-30" % "3.3.0",
     "uk.gov.hmrc"   %% "domain-play-30"                        % "11.0.0",
@@ -19,7 +19,6 @@ object AppDependencies {
     "org.scalatest"           %% "scalatest"                % "3.2.19",
     "org.scalatestplus"       %% "scalacheck-1-17"          % "3.2.18.0",
     "org.jsoup"                % "jsoup"                    % "1.21.1",
-    "org.mockito"             %% "mockito-scala-scalatest"  % "2.0.0",
     "org.scalacheck"          %% "scalacheck"               % "1.18.1",
     "org.wiremock"             % "wiremock-standalone"      % "3.13.1",
     "wolfendale"              %% "scalacheck-gen-regexp"    % "0.1.2",
