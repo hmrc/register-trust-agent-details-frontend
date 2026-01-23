ff76509097fbe3b51ab42d7b86f8a310d78a16e9
matthew-gibson-18 <175828706+matthew-gibson-18@users.noreply.github.com>
2026-01-23 13:23:19 +0000

    DDCE-7774 Migrate to new Service Navigation Component

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,13 +2,13 @@ import sbt.*
 
 object AppDependencies {
 
-  val bootstrapVersion = "10.3.0"
+  val bootstrapVersion = "10.5.0"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"   %% "play-frontend-hmrc-play-30"            % "12.18.0",
+    "uk.gov.hmrc"   %% "play-frontend-hmrc-play-30"            % "12.28.0",
     "uk.gov.hmrc"   %% "bootstrap-frontend-play-30"            % bootstrapVersion,
-    "uk.gov.hmrc"   %% "play-conditional-form-mapping-play-30" % "3.3.0",
+    "uk.gov.hmrc"   %% "play-conditional-form-mapping-play-30" % "3.4.0",
     "uk.gov.hmrc"   %% "domain-play-30"                        % "11.0.0",
     "org.typelevel" %% "cats-core"                             % "2.13.0",
     "uk.gov.hmrc"   %% "tax-year"                              % "6.0.0"
@@ -18,9 +18,9 @@ object AppDependencies {
     "uk.gov.hmrc"             %% "bootstrap-test-play-30"   % bootstrapVersion,
     "org.scalatest"           %% "scalatest"                % "3.2.19",
     "org.scalatestplus"       %% "scalacheck-1-17"          % "3.2.18.0",
-    "org.jsoup"                % "jsoup"                    % "1.21.1",
-    "org.scalacheck"          %% "scalacheck"               % "1.18.1",
-    "org.wiremock"             % "wiremock-standalone"      % "3.13.1",
+    "org.jsoup"                % "jsoup"                    % "1.22.1",
+    "org.scalacheck"          %% "scalacheck"               % "1.19.0",
+    "org.wiremock"             % "wiremock-standalone"      % "3.13.2",
     "wolfendale"              %% "scalacheck-gen-regexp"    % "0.1.2",
     "com.vladsch.flexmark"     % "flexmark-all"             % "0.64.8"
   ).map(_ % Test)
